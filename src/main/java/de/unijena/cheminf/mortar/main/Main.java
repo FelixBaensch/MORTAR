5146c55075f88d6cbea2e6f28ecaa903efc7aa36
Felix <felix.baensch@gmx.de>
2019-11-11 10:39:03 +0100

    Added main and start method to Main class

--- src/main/java/de/unijena/cheminf/mortar/main/Main.java
+++ src/main/java/de/unijena/cheminf/mortar/main/Main.java
@@ -1,4 +1,18 @@
 package main.java.de.unijena.cheminf.mortar.main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage aPrimaryStage) throws Exception {
+    }
 }
