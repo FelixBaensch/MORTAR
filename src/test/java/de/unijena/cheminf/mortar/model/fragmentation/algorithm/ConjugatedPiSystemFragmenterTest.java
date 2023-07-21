e55f047d38125f875e28daf2ddc5c0d9241c4158
MaxR <60234321+Mila1004@users.noreply.github.com>
2023-07-21 21:16:09 +0200

    WIP! started test implementation;

---
+++ src/test/java/de/unijena/cheminf/mortar/model/fragmentation/algorithm/ConjugatedPiSystemFragmenterTest.java
@@ -0,0 +1,59 @@
+/*
+ * MORTAR - MOlecule fRagmenTAtion fRamework
+ * Copyright (C) 2023  Felix Baensch, Jonas Schaub (felix.baensch@w-hs.de, jonas.schaub@uni-jena.de)
+ *
+ * Source code is available at <https://github.com/FelixBaensch/MORTAR>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.unijena.cheminf.mortar.model.fragmentation.algorithm;
+
+import javafx.beans.property.Property;
+import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
+
+/**
+ * Class to test the correct working of
+ * {@link de.unijena.cheminf.mortar.model.fragmentation.algorithm.ConjugatedPiSystemFragmenter}.
+ * ToDo: -test fragmentation
+ *
+ * @author Maximilian Rottmann
+ * @version 1.1.1.0
+ */
+public class ConjugatedPiSystemFragmenterTest {
+
+    /**
+     * Constructor that sets the default locale to british english, which is needed for correct functioning of the
+     * fragmenter as the settings tooltips are imported from the message.properties file.
+     */
+    public ConjugatedPiSystemFragmenterTest() {
+        Locale.setDefault(new Locale("en", "GB"));
+    }
+    //
+    /**
+     * Tests correct instantiation and basic settings retrieval.
+     *
+     * @throws Exception if anything goes wrong
+     */
+    @Test
+    public void basicTest() throws Exception {
+        ConjugatedPiSystemFragmenter tmpFragmenter = new ConjugatedPiSystemFragmenter();
+        System.out.println(tmpFragmenter.getFragmentationAlgorithmName());
+        for (Property tmpSetting: tmpFragmenter.settingsProperties()) {
+            System.out.println(tmpSetting.getName());
+        }
+    }
+}
