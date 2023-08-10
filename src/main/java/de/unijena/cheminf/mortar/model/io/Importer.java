0deaff5432f55ede21749fceb29272c46d5f329b
SamuelBehr <samuel.behr@gmx.de>
2023-08-10 13:58:45 +0200

    Fixed SD files not being imported in their entire length if structures fail to be read by the reader (caused the import to stop); structures imported from SDF are now assigned the file name extended with the index of the structure in the file (formerly the count of successfull imports) if no name of the structure could be detected;

--- src/main/java/de/unijena/cheminf/mortar/model/io/Importer.java
+++ src/main/java/de/unijena/cheminf/mortar/model/io/Importer.java
@@ -255,7 +255,8 @@ public class Importer {
     }
     //
     /**
-     * Imports an SD file.
+     * Imports an SD file. If no name can be detected for a structure, the file name extended with the index of the
+     * structure in the file is used as name of the structure.
      *
      * @param aFile sdf
      * @return the imported molecules in an IAtomContainerSet
@@ -266,14 +267,31 @@ public class Importer {
         IteratingSDFReader tmpSDFReader = new IteratingSDFReader(new FileInputStream(aFile),
                 SilentChemObjectBuilder.getInstance());
         int tmpCounter = 0;
-        while(!Thread.currentThread().isInterrupted() && tmpSDFReader.hasNext()){
+        while(!Thread.currentThread().isInterrupted()){
+            if (!tmpSDFReader.hasNext()) {
+                tmpSDFReader.setSkip(true);
+                if (!tmpSDFReader.hasNext()) {
+                    // there is no next
+                    break;
+                }
+                // molecule just could not be read and has therefore been skipped
+                tmpSDFReader.setSkip(false);
+                Importer.LOGGER.info("Import failed for structure:\t" + tmpCounter + " (index of structure in file).");
+                tmpCounter++;
+            }
             IAtomContainer tmpAtomContainer = tmpSDFReader.next();
             String tmpName = this.findMoleculeName(tmpAtomContainer);
-            if(tmpName == null || tmpName.isBlank() || tmpName.isEmpty())
+            if(tmpName == null || tmpName.isBlank())
+                // the counter here equals the index of the structure in the file
                 tmpName = FileUtil.getFileNameWithoutExtension(aFile) + tmpCounter;
             tmpAtomContainer.setProperty(Importer.MOLECULE_NAME_PROPERTY_KEY, tmpName);
             tmpAtomContainerSet.addAtomContainer(tmpAtomContainer);
             tmpCounter++;
+        }
+        int tmpFailedImportsCount = tmpCounter - tmpAtomContainerSet.getAtomContainerCount();
+        if (tmpFailedImportsCount > 0) {
+            Importer.LOGGER.warning("The import from SD file failed for a total of " + tmpFailedImportsCount +
+                    " structure(s).");
         }
         return tmpAtomContainerSet;
     }
