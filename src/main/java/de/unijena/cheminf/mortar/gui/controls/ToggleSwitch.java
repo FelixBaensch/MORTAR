b061b7e7e0db08f474f4b914a1b27b56d30108c9
Zeynep <zeynepdagtekin@outlook.com>
2024-05-03 19:03:47 +0200

    works now, just needs to look prettier

--- src/main/java/de/unijena/cheminf/mortar/gui/controls/ToggleSwitch.java
+++ src/main/java/de/unijena/cheminf/mortar/gui/controls/ToggleSwitch.java
@@ -1,9 +1,16 @@
 package de.unijena.cheminf.mortar.gui.controls;
 
+import javafx.animation.FillTransition;
+import javafx.animation.ParallelTransition;
 import javafx.animation.TranslateTransition;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
@@ -15,12 +22,12 @@ import javafx.util.Duration;
  * @author Zeynep Dagtekin
  * @version
  */
-public class ToggleSwitch extends Button {
+public class ToggleSwitch extends Parent {
     //<editor-fold desc="private final class constants" defaultstate="collapsed">
     /**
      * Button.
      */
-    private final Button switchButton;
+    private final Circle switchButton;
 
     /**
      * Background of the switch.
@@ -34,27 +41,40 @@ public class ToggleSwitch extends Button {
      * transition of the switch from one side to the other.
      */
     private final TranslateTransition switchAnimation;
+    private final FillTransition fillAnimation;
+    private final ParallelTransition switchTransition;
     //</editor-fold>
     /**
      * Constructor.
      */
     public ToggleSwitch() {
         //inspired by https://www.youtube.com/watch?v=maX5ymmQixM
-        this.switchButton = new Button();
-        this.switchBackground = new Rectangle(130, 15);
-        this.switchBackground.setArcWidth(15);
-        this.switchBackground.setArcHeight(15);
-        this.switchBackground.setFill(Color.LIGHTGRAY);
+        super();
+        this.switchBackground = new Rectangle(120, 18);
+        this.switchBackground.setArcWidth(20);
+        this.switchBackground.setArcHeight(20);
+        this.switchBackground.setFill(Color.WHITE);
         this.switchBackground.setStroke(Color.GRAY);
+        this.switchButton = new Circle(10);
+        this.switchButton.setCenterX(10);
+        this.switchButton.setCenterY(9);
+        this.switchButton.setFill(Color.LIGHTGRAY);
+        this.switchButton.setStroke(Color.GRAY);
         this.switchAnimation = new TranslateTransition(Duration.seconds(0.25));
         this.switchAnimation.setNode(this.switchButton);
         this.switchState = new SimpleBooleanProperty(false);
+        this.fillAnimation = new FillTransition(Duration.seconds(0.25));
+        this.switchTransition = new ParallelTransition(this.switchAnimation, this.fillAnimation);;
         this.switchAnimation.setNode(this.switchButton);
+        this.fillAnimation.setShape(this.switchBackground);
+        getChildren().addAll(this.switchBackground, this.switchButton );
         //Listener
         this.switchState.addListener((observable, oldValue, newValue) -> {
             boolean tmpIsOn = newValue.booleanValue();
-            this.switchAnimation.setToX(tmpIsOn ? (130 - 15) : 0);
-            this.switchAnimation.play();
+            this.switchAnimation.setToX(tmpIsOn ? (119 - 19) : 0);
+            this.fillAnimation.setFromValue(tmpIsOn ? Color.WHITE : Color.LIGHTBLUE);
+            this.fillAnimation.setToValue(tmpIsOn ? Color.LIGHTBLUE : Color.WHITE);
+            this.switchTransition.play();
         });
         //Mouse listener.
         setOnMouseClicked(event -> {
@@ -68,7 +88,7 @@ public class ToggleSwitch extends Button {
      *
      * @return Circle
      */
-    public Button getSwitchButton() { return this.switchButton; }
+    public Circle getSwitchButton() { return this.switchButton; }
     /**
      * returns switchBackground
      *
